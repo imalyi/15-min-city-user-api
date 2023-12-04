fca32a0c5f14d1e3039ec01b5410e6433635d576
igor <malyyigor34@gmail.com>
2023-12-04 21:31:50 +0100

    done

--- database.py
+++ database.py
@@ -1,60 +1,44 @@
-import abc
 import re
 from pymongo import MongoClient
-from pymongo.errors import DuplicateKeyError
+from pymongo.errors import DuplicateKeyError, ServerSelectionTimeoutError
 
 import logging
-from abc import ABC
 
 logger = logging.getLogger(f"{__name__}_Database")
-MONGO_CONNECT = "mongodb://gmaps:gmaps@192.168.0.100:28017/"
+
+MONGO_CONNECT = "mongodb://gmaps:gmaps@bed:28017/"
 MONGO_DB_NAME = "15min"
 MONGO_DB_HOST = 123
 
 
-class Database(ABC):
-    @abc.abstractmethod
-    def add_item(self, item):
-        pass
-
-
-class DummyDatabase(Database):
-    def add_item(self, item):
-        print(item)
-
-
-class MongoDatabase(Database):
+class MongoDatabase():
     def __init__(self):
         self.__connect()
 
     def __connect(self):
-        self.client = MongoClient()
-        self.client = MongoClient(MONGO_CONNECT)
-        self.db = self.client.get_database(MONGO_DB_NAME)
-
-    def add_item(self, data):
         try:
-            logger.debug(f"Creating document {data}")
-            self.db['address'].insert_one(data.copy())
-            logger.debug(f"Document created")
-        except DuplicateKeyError:
-            logger.debug(f"Document exist in db")
+            self.client = MongoClient(MONGO_CONNECT, serverSelectionTimeoutMS=2000)
+            self.db = self.client.get_database(MONGO_DB_NAME)
+        except ServerSelectionTimeoutError:
+            logger.error("Failed to connect to MongoDB server")
 
-    def search(self, address: str):
-        queryes = []
-        for part in address.split(" "):
-            pattern = re.compile(f".*{part}.*", re.IGNORECASE)
-            queryes.append({"properties.full_address": {"$regex": pattern}})
+    def search_by_partial_name(self, address: str):
+        queries = [{"full": {"$regex": re.compile(f'.*{part}.*', re.IGNORECASE)}} for part in address.split()]
+        try:
+            result = self.db['address'].find({"$and": queries}).limit(5)
+        except Exception as e:
+            logger.error(f"Error executing MongoDB query: {e}")
+            return []
 
-        result = self.db['gmaps'].find({
-            "$and":
-                queryes
-        }).limit(4)
-        return result or []
+        return [{"address": doc.get('full'), "id": str(doc.get('_id'))} for doc in result]
 
-
-def get_database() -> Database:
-    if MONGO_DB_HOST is None:
-        return DummyDatabase()
-    else:
-        return MongoDatabase()
+    def search_by_coordinates(self, lon: float, lat: float):
+        query = {
+            "location": {
+                "$geoWithin": {
+                    "$centerSphere": [[lon, lat], 0.013 / 6371]
+                }
+            }
+        }
+        result = self.db['address'].find(query).limit(5)
+        return [{"address": doc.get('full'), "id": str(doc.get('_id'))} for doc in result]
--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-from fastapi import FastAPI
-import uvicorn
+import logging
+from fastapi import Depends, FastAPI
 from database import MongoDatabase
+import uvicorn
+
 app = FastAPI()
 
-DATABASE = MongoDatabase()
+
+def get_database():
+    return MongoDatabase()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+logging.basicConfig(level=logging.INFO)
 
 
 @app.get("/address/from/name/{name}")
-async def say_hello(name: str):
-    return DATABASE.search(name)
+async def get_address_by_name(name: str, database: MongoDatabase = Depends(get_database)):
+    results = database.search_by_partial_name(name)
+    logging.info(results)
+    return results
+
+
+@app.get("/address/from/coordinates/{lon}/{lat}")
+async def get_address_by_coordinates(lon: float, lat: float, database: MongoDatabase = Depends(get_database)):
+    results = database.search_by_coordinates(lon, lat)
+    logging.info(results)
+    return results
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)
