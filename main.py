718f55eded71af7db5d88eb99bb2d08e615c9b18
igor <malyyigor34@gmail.com>
2023-12-09 22:26:37 +0100

    init

--- main.py
+++ main.py
@@ -8,14 +8,12 @@ from starlette.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 
-
-
-
 app.add_middleware(middleware_class=CORSMiddleware,
     allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],)
+
 
 def get_database():
     return MongoDatabase()
@@ -35,6 +33,12 @@ async def get_address_by_name(name: str=None, lon: float=None, lat: float=None, 
 
     logging.info(results)
     return results
+
+
+@app.get('/report/')
+async def get_report(address_id: str):
+    pass
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)
