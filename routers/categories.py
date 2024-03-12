a9b69720b47dcf316c315a3c0c91aa0500c4aa61
igor <malyyigor34@gmail.com>
2024-03-12 22:20:04 +0100

    fix bug with imports

--- routers/categories.py
+++ routers/categories.py
@@ -4,22 +4,11 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from fastapi import APIRouter
 from database.mongo_database import MongoDatabase
-
+from database.get_database import get_database
 
 logging.basicConfig(level=logging.INFO)
 
-app.add_middleware(middleware_class=CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],)
-
 router = APIRouter()
-
-
-def get_database():
-    return MongoDatabase()
-
 
 @router.get("/")
 async def get_categories(partial_name: str = None, database: MongoDatabase = Depends(get_database)):
