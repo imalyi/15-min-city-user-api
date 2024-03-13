22a4e9461f3454a5141dfd470415ee230a225dad
igor malyi <93244697+imalyi@users.noreply.github.com>
2024-03-13 12:08:49 +0100

    cors allow all

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ from fastapi import FastAPI
 from routers import report
 from routers import categories
 from routers import address
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(middleware_class=CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],)
 
 app.include_router(report.router, prefix='/report', tags=['Report'])
 app.include_router(categories.router, prefix="/categories", tags=["Categories"])
