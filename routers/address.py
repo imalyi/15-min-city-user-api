46c261b18999575636cd2fbc9d714f8980c888f1
igor malyi <igor.malyi@pm.me>
2024-04-23 08:54:26 +0200

    Scrum 100 elasticsearch dla przechowywania adresow (#46)
    add es

--- routers/address.py
+++ routers/address.py
@@ -6,10 +6,11 @@ from database.get_database import get_database
 from fastapi import FastAPI, HTTPException
 from database.model import AddressIn, AddressOut
 from typing import List
+from database.es import fuzzy_search
 
 router = APIRouter()
 
 @router.get("/")
 async def get_address(q: AddressIn = Depends(), database: MongoDatabase = Depends(get_database)) -> List[str]:
-    results = database.search_by_partial_name(q.name)
+    results = fuzzy_search(q.name)
     return results
