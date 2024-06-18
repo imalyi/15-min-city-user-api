1e89e4105fe48fec4fe599c611011c996eda4abf
Igor Malyi <igor.malyi@pm.me>
2024-06-18 21:26:30 +0200

    fix

--- heatmap.py
+++ heatmap.py
@@ -19,5 +19,6 @@ router = APIRouter()
 
 @router.post("/")
 async def generate_heatmap(categories: List[Category], background_tasks: BackgroundTasks, database: MongoDatabase = Depends(get_database)):
+    print(categories)
     task = generate_heatmap_task.delay(categories)
     return {"task_id": task.id}
