09f1b0c96183b33ee206b130c96592780d807b62
Igor Malyi <igor.malyi@pm.me>
2024-06-18 23:12:23 +0200

    fix bug

--- heatmap.py
+++ heatmap.py
@@ -29,6 +29,7 @@ def generate_cache_key(categories: List[dict]) -> str:
 
 
 
+@router.get("/")
 async def generate_heatmap(categories: List[Category], background_tasks: BackgroundTasks, database: MongoDatabase = Depends(get_database)):
     categories_dict = [category.dict() for category in categories]
     # Generate cache key from sorted categories
