5582e3ba54a33c404904386e167049d5722e2aaf
Igor Malyi <igor.malyi@pm.me>
2024-06-18 16:59:34 +0200

    fix

--- routers/celery_app.py
+++ routers/celery_app.py
@@ -17,4 +17,35 @@ celery_app.conf.update(
     enable_utc=True,
 )
 
-celery_app.register_task(generate_heatmap_task)
+
+def ensure_closed_ring(ring):
+    if ring[0] != ring[-1]:
+        ring.append(ring[0])
+    return ring
+
+class HeatMapModel:
+    def __init__(self):
+        self._db = MongoDatabase().db
+
+    def generate(self, required_categories: List[Category]):
+        conditions = {}
+        for c in required_categories:
+            conditions[f"points_of_interest.{c['main_category']}.{c['category']}"] = {"$ne": []}
+        results = self._db['address'].find(conditions)
+        features = []
+        for i, doc in enumerate(results):
+            geometry = {
+                "type": "Polygon",
+                "coordinates": [ensure_closed_ring(ring) for ring in doc['geometry']]
+            }
+            feature = geojson.Feature(geometry=geometry)
+            features.append(feature)
+            if i % 1000 == 0:
+                print(f"Handled {i} docs")
+        feature_collection = geojson.FeatureCollection(features)
+        return geojson.loads(geojson.dumps(feature_collection))
+
+@celery_app.task(name="generate_heatmap_task")
+def generate_heatmap_task(categories):
+    #h = HeatMapModel()
+    return 1*10000000#h.generate(categories)
