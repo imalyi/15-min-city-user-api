bd529f65ae5e69ba4a6fee5770797c4fb345d515
Igor Malyi <igor.malyi@pm.me>
2024-10-06 11:07:01 +0200

    five free sub for every user

--- api/users/subscriptions/free_trial.py
+++ api/users/subscriptions/free_trial.py
@@ -15,3 +15,15 @@ async def give_free_trial(user_id):
     }
 
     await UserSubscriptionDAO.insert_data(data)
+
+async def give_free_sub(user_id):
+    free_sub = await SubscriptionDAO.find_one_or_none(order_by=None, title = "free")
+    data = {
+        'user_id': user_id,
+        'subscription_level': free_sub.level,
+        'date_from': date.today(),
+        'date_to': date.today() + timedelta(days=99999)
+    }
+
+    await UserSubscriptionDAO.insert_data(data)
+
